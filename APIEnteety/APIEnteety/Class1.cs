a0ac44610bef139ea917f3297a8ab4df0466bfc8
MIchaell22 <55326207+MIchaell22@users.noreply.github.com>
2020-04-06 12:35:10 +0300

    1
    1

---
+++ APIEnteety/APIEnteety/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIEnteety
+{
+    class Class1
+    {
+        public class ApplicationContext : DbContext
+        {
+            public DbSet<Passengers> Passengers { get; set; }
+            public DbSet<Flinghts> Flinghts { get; set; }
+
+            public ApplicationContext()
+            {
+                Database.EnsureCreated();
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Flightsdb;Trusted_Connection=True;");
+            }
+        }
+    }
+}
---
+++ APIEnteety/APIEnteety/Flinght.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+namespace APIEnteety
+{
+    public class Flinghts
+    {
+        [Key]
+        public int Flight_number { get; set; }
+        public string Froms { get; set; }
+        public string Wheres { get; set; }
+        public string Airlines { get; set; }
+        public int Length_km { get; set; }
+        public string Time_in_path { get; set; }
+        public string Aircraft_type { get; set; }
+
+    }
+}
